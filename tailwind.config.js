a70d742f5bd843a2f4050319ca0baa5dff96a346
daodao97 <daodao97@foxmail.com>
2025-03-16 20:50:25 +0800

    up

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,16 +2,9 @@ module.exports = {
     content: [
         "./frontend/**/**/*.{js,jsx,ts,tsx}",
         "./modules/server/templates/*.{html,js,jsx,ts,tsx}",
-        "./locales/**/*.{json}"
+        "./locales/**/*.json"
     ],
     theme: {
         darkMode: 'class',
-        extend: {
-            colors: {
-                primary: {
-                },
-            }
-        }
     },
-    plugins: [],
 }; 
